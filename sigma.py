86be868f364646c4ce50ddd0c47c9abb0e4c5d45
FurnaceDev <126364275+thateggdev@users.noreply.github.com>
2024-02-16 18:44:30 -0500

    Add files via upload

---
+++ sigma.py
@@ -0,0 +1,10 @@
+import time
+
+def WhatsTheSigmaMeal():
+    print("is your sigma meal... " + yoursigmameal + "?")
+    time.sleep(1)
+    print("very cool")
+
+yoursigmameal = input("sigma meal: ")
+
+WhatsTheSigmaMeal()
